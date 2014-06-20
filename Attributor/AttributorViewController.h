0dd73dab74e0e4b41f9793c75084d0cbefced1c4
Jared Smith <jared@zenpayroll.com>
2014-06-19 23:13:58 -0700

    Initial Commit

---
+++ Attributor/AttributorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributorViewController.h
+//  Attributor
+//
+//  Created by Jared Smith on 6/19/14.
+//  Copyright (c) 2014 jaredsmithse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+@end
---
+++ Attributor/AttributorViewController.m
@@ -0,0 +1,29 @@
+//
+//  AttributorViewController.m
+//  Attributor
+//
+//  Created by Jared Smith on 6/19/14.
+//  Copyright (c) 2014 jaredsmithse. All rights reserved.
+//
+
+#import "AttributorViewController.h"
+
+@interface AttributorViewController ()
+
+@end
+
+@implementation AttributorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
